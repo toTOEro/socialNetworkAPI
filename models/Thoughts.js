ca3aa1e17cfd1562c820102108207cbe3bb0f6c3
anthony3738-miZJgd <anthony3738@gmail.com>
2022-10-16 14:21:41 -0700

    Cleaned comments

--- models/Thoughts.js
+++ models/Thoughts.js
@@ -11,9 +11,7 @@ const thoughtSchema = new mongoose.Schema({
     thoughtText: { type: String, required: true, maxLength: 280 },
     username: { type: String, required: true },
     createdAt: { type: Date, default: Date.now, get: formatDate },
-    // reactions: [[{ type: mongoose.Schema.Types.ObjectId, ref: 'Reaction' }]],
     reactions: [reactionSchema]
-    // reactionCount: Number,
 
 });
 
