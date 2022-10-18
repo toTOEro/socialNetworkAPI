1274f399edccb7e41ab8913ed8b4d7b14e264ce5
anthony3738-miZJgd <anthony3738@gmail.com>
2022-10-17 19:20:40 -0700

    fixed minlength

--- models/Reactions.js
+++ models/Reactions.js
@@ -8,13 +8,13 @@ const formatDate = function (date) {
 
 const reactionSchema = new mongoose.Schema({
 
-    reactionId: 
+    reactionId:
     {
-        type: mongoose.Schema.Types.ObjectId, 
+        type: mongoose.Schema.Types.ObjectId,
         default: () => new mongoose.Types.ObjectId(),
     },
 
-    reactionBody: { type: String, required: true, maxLength: 280 },
+    reactionBody: { type: String, required: true, minLength: 1, maxLength: 280 },
     username: { type: String, required: true },
     createdAt: { type: Date, default: Date.now, get: formatDate },
 
